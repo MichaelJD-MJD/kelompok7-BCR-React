6cddeb049ae5ad99375d88eb47167e14741d9874
Michael JD <michaeljdarwin9@gmail.com>
2024-11-08 13:54:48 +0700

    [Fixing] Organize project structure by dividing backend and frontend

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import slices from "./slices";
-
-// it will make temporary global state (FE Database)
-export const store = configureStore({
-    reducer: slices,
-    devTools: import.meta.env.MODE == "development",
-}); 
